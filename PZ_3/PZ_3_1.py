6c125da02d7dd43da5e933543c96e9880ac0d3d8
Artem200715 <matartem07@gmail.com>
2024-11-22 17:39:57 +0300

    сделал часть 3 ПР

---
+++ PZ_3/PZ_3_1.py
@@ -0,0 +1,24 @@
+# Проверить истинность высказывания: «Среди трех данных целых чисел есть хотя бы
+# одна пара совпадающих».
+
+number1, number2, number3 = input('Введите первое число'), input('Введите второе число'), input('Введите третье число')
+
+while type(number1) != int:
+    try:
+        number1 = int(number1)
+    except ValueError:
+        print('Неправильно Ввели!')
+        number1 = input('Введите первое число')
+while type(number2) != int:
+    try:
+        number2 = int(number2)
+    except ValueError:
+        print('Неправильно Ввели!')
+        number2 = input('Введите второе число')
+while type(number3) != int:
+    try:
+        number3 = int(number3)
+    except ValueError:
+        print('Неправильно Ввели!')
+        number3 = input('Введите третье число')
+
