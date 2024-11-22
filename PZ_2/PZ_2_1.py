d662d6fd7572c888deccf7bb2e1aec6c05ee87a5
Artem200715 <matartem07@gmail.com>
2024-11-22 17:26:43 +0300

    сделал 2 ПР

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,9 +1,12 @@
+# Дано трехзначное число. В нем зачеркнули первую справа цифру и
+# ее слева. Вывести полученное число.
+
 number = int(input('Введите трехзначное число'))
 
-last = number%10
+last = number % 10
 
-other = number//10
+other = number // 10
 
-chislo = last*100+other
+chislo = last * 100 + other
 
 print(chislo)
