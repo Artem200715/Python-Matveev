cef68c3505400f1a4c451af6fba02b72dcda1fc4
Artem200715 <matartem07@gmail.com>
2025-01-24 16:20:22 +0300

    сделал 9 ПЗ

--- PZ-9/PZ_9_1.py
+++ PZ-9/PZ_9_1.py
@@ -2,14 +2,13 @@
 # из трех моделей в качестве значений. Обеспечить отображение вторых моделей по
 # каждому авто, всех моделей словаря.
 
-avto = {1: ["iX M60", "i7", "i4 M50 xDrive"], 2: ["Tiguan", "Passat", "Polo"], 3: ["Octavia", "Fabia", "Rapid"]}
+avto = {'BMW': ["iX M60", "i7", "i4 M50 xDrive"], 'Wolksvagen': ["Tiguan", "Passat", "Polo"], 'Skoda': ["Octavia", "Fabia", "Rapid"]}
 
-k = 1
-while k != 4:
-    mark = avto.get(k)
-    print(mark[1])
-    k += 1
-k = 1
-while k != 4:
-    print(avto.get(k))
-    k += 1
+for key in avto:
+    mark = avto.get(key)
+    print(key, mark[1])
+
+
+print(avto['BMW'])
+print(avto['Wolksvagen'])
+print(avto['Skoda'])
