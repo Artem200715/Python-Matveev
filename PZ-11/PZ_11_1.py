7e7e425a86033b52d62e55da01f09a26cf4cb042
Artem200715 <matartem07@gmail.com>
2025-03-07 12:50:25 +0300

    сделал 12 ПЗ

---
+++ PZ-11/PZ_11_1.py
@@ -0,0 +1,23 @@
+# Средствами языка Python сформировать текстовый файл (.txt), содержащий
+# последовательность из целых положительных и отрицательных чисел. Сформировать
+# новый текстовый файл (.txt) следующего вида, предварительно выполнив требуемую
+# обработку элементов:
+# Исходные данные:
+# Количество элементов:
+# Сумма элементов:
+# Элементы до n-1 умножены на элемент n:
+
+l = ['-50 -25 0 25 50']
+
+file = open('matveev.txt', 'w', encoding="utf-8")
+file.writelines(l)
+file.close()
+file1 = open('matveev1.txt', 'w', encoding="utf-8")
+file1.writelines('Исходные данные: ')
+file1.writelines(l)
+file1.write('\n')
+file1.close()
+file1 = open('matveev1.txt', 'a', encoding="utf-8")
+file1.writelines('Количество элементов: ')
+file1.write('\n')
+file1.close()
---
+++ PZ-12/PZ_12.1.py
@@ -0,0 +1,5 @@
+num = [1, 5, 2, 1, 7, 12, 4, 3, 9]
+x = 0
+for i in range(0, 3):
+    x += num[i]
+print(x)
