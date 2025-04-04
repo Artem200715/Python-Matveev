1873b31949992ad7952c5e10c3e54dce7c412355
Artem200715 <matartem07@gmail.com>
2025-04-04 12:00:56 +0300

    Доделал 13

--- PZ-13/PZ_13.1.py
+++ PZ-13/PZ_13.1.py
@@ -1,13 +1,15 @@
 # В двумерном списке найти среднее арифметическое элементов последних двух
 # столбцов.
 from functools import reduce
-number = [[1, 6, 2],
-          [2, 4, 0],
-          [8, 7, 5]]
-
+import random
+number = [[random.randint(1, 10) for x in range(1, 4)] for y in range(random.randint(3, 5))]
+print("Список: ", number)
 answ = []
 
 for i in number:
     answ += i[1:3]
 
-print(reduce(lambda x, y: x+y, answ))
+summ = reduce(lambda x, y: x+y, answ)
+print("Сумма последних 2 столбцов: ", summ)
+mid = summ / len(answ)
+print("Среднее арифметическое элементов двух последних столбцов", mid)
